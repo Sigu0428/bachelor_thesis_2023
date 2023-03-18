19195ab05090dd8d7b97eeac77087f77456fa577
sigu0428 <sigurdjagerlemming@gmail.com>
2023-03-18 08:27:15 +0100

    renaming and panda default launch file

--- mycode/src/genPointCloud.py
+++ mycode/src/genPointCloud.py
@@ -15,37 +15,38 @@ from sensor_msgs.msg import ChannelFloat32
 pub = rospy.Publisher('workspacePointCloud', PointCloud, queue_size=1, latch=True)
 rospy.init_node('urdfToWorkspace', anonymous=True)
 
-END_EFFECTOR = "panda_link8"
-BASE =  "panda_link0_sc"
-URDF_PATH = "/home/philip/catkin_ws/src/Afrovenator/mycode/src/panda_generated.urdf"
+END_EFFECTOR = rospy.get_param("end_effector_name") # "panda_link8"
+BASE = rospy.get_param("base_name") # "panda_link0_sc"
+MODEL = rospy.get_param("robot_description") # "/home/sigurd/catkin_ws/src/mycode/src/panda_generated.urdf"
+JOINT_LIMITS = rospy.get_param("joint_limit_yaml")
 
 # if gpu available, set devices
 d = "cuda" if torch.cuda.is_available() else "cpu"
 dtype = torch.float32
 
 # load robot description from URDF and specify end effector link
-chain = pk.build_serial_chain_from_urdf(open(URDF_PATH).read(), END_EFFECTOR)
+chain = pk.build_serial_chain_from_urdf(MODEL, END_EFFECTOR)
+#chain = pk.build_serial_chain_from_urdf(open("/home/sigurd/catkin_ws/src/mycode/src/panda_generated.urdf").read(), END_EFFECTOR)
 
 # Print kinematic chain
 print(chain)
 
-print(chain.get_joint_parameter_names()[0])
+print(chain.get_frame_names())
 
-N = 100000
+N = 10000
 # N Uniform random configuration [0, 1]
 th_batch = torch.rand(N, len(chain.get_joint_parameter_names()), dtype=dtype, device=d)
 
 # Load yaml file with joint limits
-with open("/home/philip/catkin_ws/src/Afrovenator/mycode/src/joint_limits.yaml", "r") as stream:
-    try:
-        joint_data = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+try:
+    joint_data = yaml.safe_load(JOINT_LIMITS)
+except yaml.YAMLError as exc:
+    print(exc)
 
 # Make uniform random configurations [0, 1] correspond to limits [lower, upper]
 for x in th_batch:
     for i, value in enumerate(joint_data.values()):
-        x[i] *= value["limit"]["upper"] - value["limit"]["lower"]
+        x[i] *= (value["limit"]["upper"] - value["limit"]["lower"])
         x[i] += value["limit"]["lower"]
 
 # Generate forward kinematics 3D transform N x 2D transforms
@@ -54,11 +55,13 @@ ret = chain.forward_kinematics(th_batch, end_only=True)
 # 3D jacobian N x 2D jacobians and svd
 J = chain.jacobian(th_batch)
 svd = torch.svd(J)
+print(svd)
 
 singular_metric = ChannelFloat32()
 singular_metric.name = "singular metric"
 for x in svd.S:
-    singular_metric.values.append(x[-1])
+    singular_metric.values.append(x[-1] / x[0])
+    #singular_metric.values.append(x[0] * x[1] * x[2] * x[3] * x[4] * x[5])
 
 points = torch.zeros(N, 1, 3)
 tf_points = ret.transform_points(points)
@@ -76,5 +79,6 @@ for i, [x] in enumerate(tf_points.numpy()):
 
 pointCloud.channels = [singular_metric]
 
-
 pub.publish(pointCloud)
+
+rospy.spin()
